1fa16c52dc709d8ccf25c7004f95bba536b11141
Alex Jover Morales <alex.jovermorales@coosto.com>
2017-02-07 11:01:59 +0100

    build(gh-pages-publish): Add a .nojekyll root file

--- tools/gh-pages-publish.js
+++ tools/gh-pages-publish.js
@@ -1,4 +1,4 @@
-let { cd, exec, echo } = require('shelljs')
+let { cd, exec, echo, touch } = require('shelljs')
 let { readFileSync } = require('fs')
 let url = require('url')
 
@@ -8,13 +8,12 @@ repository = repository.host + repository.path
 let ghToken = process.env.GH_TOKEN
 
 echo('Deploying docs!!!')
-echo(`${repository} ${ghToken}`)
 cd('dist/docs')
-exec('pwd')
+touch('.nojekyll')
 exec('git init')
 exec('git config user.name "Alex Jover Morales"')
 exec('git config user.email "alexjovermorales@gmail.com"')
 exec('git add .')
 exec('git commit -m "docs(docs): update gh-pages"')
-exec(`git push --force "https://${ghToken}@${repository}" master:gh-pages`)
+exec(`git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`)
 echo('Docs deployed!!')
