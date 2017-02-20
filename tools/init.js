77dcaf5b241012c5030cf2da14c515d8ba375b65
ciroreed <ciroreed@gmail.com>
2017-02-20 20:40:05 +0100

    fix #1 postinstall script now replaces default username

--- tools/init.js
+++ tools/init.js
@@ -20,7 +20,7 @@ const promptSchema = {
   }
 }
 
-const files = ['package.json', 'webpack.config.ts']
+const files = ['package.json', 'webpack.config.ts', 'LICENSE']
 
 prompt.start()
 prompt.message = ''
@@ -30,6 +30,11 @@ if (!which('git')) {
   console.log(colors.red('Sorry, this script requires git'))
   process.exit(1)
 }
+
+let username, usermail
+
+username = exec('git config user.name').stdout.trim()
+usermail = exec('git config user.email').stdout.trim()
 
 // Clear console
 let lines = process.stdout.getWindowSize()[1];
@@ -46,38 +51,40 @@ console.log('\r\n', 'Removed .git directory','\r\n')
 
 // 2. Ask for library name
 prompt.get(promptSchema, (err, res) => {
-  if (!err) {
-    // 3. Replace strings in corresponding files
-    replace({
-      files,
-      from: /--libraryname--/g,
-      to: res.library
-    }, () => {
-      // 4. Rename main file and test
-      const renamedFiles = [`src/${res.library}.ts`, `test/${res.library}.test.ts`]
-      mv(path.resolve(__dirname, '..', 'src/library.ts'), path.resolve(__dirname, '..', renamedFiles[0]))
-      mv(path.resolve(__dirname, '..', 'test/library.test.ts'), path.resolve(__dirname, '..', renamedFiles[1]))
-
-      console.log()
-      console.log(colors.cyan(renamedFiles.join(',')) + ' renamed')
-      console.log(colors.cyan(files.join(',')) + ' updated')
-
-      // 5. Recreate init folder and initialize husky
-      exec('git init ' + path.resolve(__dirname, '..'))
-      fork(path.resolve(__dirname, '..', 'node_modules', 'husky', 'bin', 'install'))
-
-      // 6. Remove post-install command
-      const pkg = JSON.parse(readFileSync(path.resolve(__dirname, '..', 'package.json')))
-      delete pkg.scripts.postinstall
-      writeFileSync(path.resolve(__dirname, '..', 'package.json'), JSON.stringify(pkg, null, 2))
-      console.log()
-      console.log(colors.cyan('Removed postinstall script'))
-      console.log()
-
-      console.log()
-      console.log(colors.green('Happy coding!! ;)'))
-      console.log()
-    })
+  if (err) {
+    console.log(colors.red('There was an error building the workspace :('))
+    process.exit(1)
+    return
   }
-})
+  // 3. Replace strings in corresponding files
+  replace({
+    files,
+    from: [/--libraryname--/g, /--username--/g, /--usermail--/g],
+    to: [res.library, username, usermail]
+  }, () => {
+    // 4. Rename main file and test
+    const renamedFiles = [`src/${res.library}.ts`, `test/${res.library}.test.ts`]
+    mv(path.resolve(__dirname, '..', 'src/library.ts'), path.resolve(__dirname, '..', renamedFiles[0]))
+    mv(path.resolve(__dirname, '..', 'test/library.test.ts'), path.resolve(__dirname, '..', renamedFiles[1]))
 
+    console.log()
+    console.log(colors.cyan(renamedFiles.join(',')) + ' renamed')
+    console.log(colors.cyan(files.join(',')) + ' updated')
+
+    // 5. Recreate init folder and initialize husky
+    exec('git init ' + path.resolve(__dirname, '..'))
+    fork(path.resolve(__dirname, '..', 'node_modules', 'husky', 'bin', 'install'))
+
+    // 6. Remove post-install command
+    const pkg = JSON.parse(readFileSync(path.resolve(__dirname, '..', 'package.json')))
+    delete pkg.scripts.postinstall
+    writeFileSync(path.resolve(__dirname, '..', 'package.json'), JSON.stringify(pkg, null, 2))
+    console.log()
+    console.log(colors.cyan('Removed postinstall script'))
+    console.log()
+
+    console.log()
+    console.log(colors.green('Happy coding!! ;)'))
+    console.log()
+  })
+})
