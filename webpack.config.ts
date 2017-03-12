5325ef73a63a618f6a454aa7e1c2311f8107b34c
Chi <contractswif@hotmail.com>
2017-03-12 21:46:44 +0800

    feat(webpack): Add webpack-dev-server, add yarn.lock

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,30 @@
-import { join } from 'path'
+import { join, resolve } from 'path'
 const { camelCase } = require('lodash')
+const webpack = require("webpack")
 const { TsConfigPathsPlugin, CheckerPlugin } = require('awesome-typescript-loader')
 const TypedocWebpackPlugin = require('typedoc-webpack-plugin')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 /**
  * Update this variable if you change your library name
  */
 const libraryName = '--libraryname--'
+const env = process && process.env && process.env.NODE_ENV;
+const dev = !(env && env === 'production');
+const entry = dev ? [
+      // 'react-hot-loader/patch',
+      'webpack-dev-server/client?http://localhost:8081',
+      // bundle the client for webpack-dev-server
+      // and connect to the provided endpoint
+      'webpack/hot/only-dev-server',
+      // bundle the client for hot reloading
+      // only- means to only hot reload for successful updates
+      `./src/${libraryName}.ts`
+    ] : join(__dirname, `src/${libraryName}.ts`)
 
 export default {
-  entry: join(__dirname, `src/${libraryName}.ts`),
+  entry: {
+      index: entry
+  },
   // Currently cheap-module-source-map is broken https://github.com/webpack/webpack/issues/4176
   devtool: 'source-map',
   output: {
@@ -35,7 +50,16 @@ export default {
   },
   plugins: [
     new CheckerPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new TsConfigPathsPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      title: libraryName,
+      filename: 'index.html',
+      template: join(__dirname, 'src/template/common.html'),
+      hash: true,
+      chunks: ['common', 'index']
+    }),
     new TypedocWebpackPlugin(
       {
         theme: 'minimal',
@@ -45,5 +69,10 @@ export default {
       },
       'src'
     )
-  ]
+  ],
+  devServer: {
+      hot: true,
+      contentBase: resolve(__dirname, 'dist'),
+      publicPath: '/'
+  }
 }
