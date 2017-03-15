1518f9201b938f37e954ec118921a9094ab4f37a
Chi <contractswif@hotmail.com>
2017-03-15 13:40:54 +0800

    style(webpack): Code style (whitespace mainly) clean up

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,26 +4,52 @@ const webpack = require("webpack")
 const { TsConfigPathsPlugin, CheckerPlugin } = require('awesome-typescript-loader')
 const TypedocWebpackPlugin = require('typedoc-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const env = process && process.env && process.env.NODE_ENV
+const dev = !(env && env === 'production')
 /**
  * Update this variable if you change your library name
  */
 const libraryName = '--libraryname--'
-const env = process && process.env && process.env.NODE_ENV;
-const dev = !(env && env === 'production');
-const entry = dev ? [
-      // 'react-hot-loader/patch',
-      'webpack-dev-server/client?http://localhost:8081',
-      // bundle the client for webpack-dev-server
-      // and connect to the provided endpoint
-      'webpack/hot/only-dev-server',
-      // bundle the client for hot reloading
-      // only- means to only hot reload for successful updates
-      `./src/${libraryName}.ts`
-    ] : join(__dirname, `src/${libraryName}.ts`)
+const plugins = [
+  new CheckerPlugin(),
+  new webpack.HotModuleReplacementPlugin(),
+  new TsConfigPathsPlugin(),
+  new HtmlWebpackPlugin({
+    inject: true,
+    title: libraryName,
+    filename: 'index.html',
+    template: join(__dirname, 'src/template/common.html'),
+    hash: true,
+    chunks: ['common', 'index']
+  })
+]
+let entry: string | string[] = [
+  // 'react-hot-loader/patch',
+  'webpack-dev-server/client?http://localhost:8081',
+  // bundle the client for webpack-dev-servers
+  // and connect to the provided endpoint
+  'webpack/hot/only-dev-server',
+  // bundle the client for hot reloading
+  // only- means to only hot reload for successful updates
+  `./src/${libraryName}.ts`
+]
+
+if (dev === false) {
+  plugins.push(new TypedocWebpackPlugin(
+    {
+      theme: 'minimal',
+      out: 'docs',
+      target: 'es6',
+      ignoreCompilerErrors: true
+    },
+    'src'
+  ))
+  entry = join(__dirname, `src/${libraryName}.ts`)
+}
 
 export default {
   entry: {
-      index: entry
+    index: entry
   },
   // Currently cheap-module-source-map is broken https://github.com/webpack/webpack/issues/4176
   devtool: 'source-map',
@@ -48,31 +74,10 @@ export default {
       }
     ]
   },
-  plugins: [
-    new CheckerPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new TsConfigPathsPlugin(),
-    new HtmlWebpackPlugin({
-      inject: true,
-      title: libraryName,
-      filename: 'index.html',
-      template: join(__dirname, 'src/template/common.html'),
-      hash: true,
-      chunks: ['common', 'index']
-    }),
-    new TypedocWebpackPlugin(
-      {
-        theme: 'minimal',
-        out: 'docs',
-        target: 'es6',
-        ignoreCompilerErrors: true
-      },
-      'src'
-    )
-  ],
+  plugins: plugins,
   devServer: {
-      hot: true,
-      contentBase: resolve(__dirname, 'dist'),
-      publicPath: '/'
+    hot: true,
+    contentBase: resolve(__dirname, 'dist'),
+    publicPath: '/'
   }
 }
