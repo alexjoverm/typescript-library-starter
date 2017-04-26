03515d7dd3ae2f713ab66de86d4867a59ae11043
Steve Lee <steve@opendirective.com>
2017-04-26 07:55:47 +0100

    Add code to delete tools/init.js. See: #43

--- tools/init.js
+++ tools/init.js
@@ -83,6 +83,11 @@ console.log(colors.yellow('Hi! I\'m setting things up for you!!'))
 rm('-Rf', path.resolve(__dirname, '..', '.git'))
 console.log('\r\n', 'Removed .git directory','\r\n')
 
+// Remove files
+const files_rm = ['tools/init.js']
+const paths_rm = files_rm.map((f) => path.resolve(__dirname, '..', f))
+rm(paths_rm)
+console.log(`\r\nRemoved files: ${files_rm.toString()}\r\n`)
 
 if (!inCI) {
   // Ask for library name
