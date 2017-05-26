c9af55a0b91bc5a155889e10eaaf5cffcdd06aad
Alex J <alexjovermorales@gmail.com>
2017-05-26 17:55:29 +0200

    Remove Uglify Rollup plugin from rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,10 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
 const pkg = require('./package.json')
 const { camelCase } = require('lodash')
 
 const libraryName = '--libraryname--'
-
-let plugins = [
-  [
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't undestand cjs)
-    commonjs(),
-     // Allow node_modules resolution, so you can use 'external' to control
-     // which external modules to include in the bundle
-    resolve(),
-    // Don't transpile node_modules. You may change this if you wanna transpile something in there
-    babel({ exclude: 'node_modules/**' })
-  ]
-]
-
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(uglify())
-}
 
 export default {
   entry: `compiled/${libraryName}.js`,
@@ -32,5 +15,13 @@ export default {
   sourceMap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
-  plugins
+  plugins: [
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't undestand cjs)
+    commonjs(),
+     // Allow node_modules resolution, so you can use 'external' to control
+     // which external modules to include in the bundle
+    resolve(),
+    // Don't transpile node_modules. You may change this if you wanna transpile something in there
+    babel({ exclude: 'node_modules/**' })
+  ]
 }
