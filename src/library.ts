653b27606af823d067f02c6d72cf55edec5cbd79
Alex J <alexjovermorales@gmail.com>
2017-06-18 16:55:33 +0200

    Remove Set line from src/library

--- src/library.ts
+++ src/library.ts
@@ -1,9 +1,6 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
-
-const a = new Set<number>()
-
 export default class DummyClass {
 
 }
