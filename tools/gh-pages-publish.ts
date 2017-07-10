a8e449df89c891e8b2a830f3c44211b9c9b602c5
Alex J <alexjovermorales@gmail.com>
2017-07-10 18:44:53 +0200

    Fix git user hardcoded. Closes #68

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -22,8 +22,8 @@ cd("dist/docs")
 touch(".nojekyll")
 exec("git init")
 exec("git add .")
-exec('git config user.name "Alex J"')
-exec('git config user.email "alexjovermorales@gmail.com"')
+exec('git config user.name "--username--"')
+exec('git config user.email "--usermail--"')
 exec('git commit -m "docs(docs): update gh-pages"')
 exec(
   `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
