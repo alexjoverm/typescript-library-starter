f25ade1887d64548fa08ef7ec1cf22fe1c9b56ba
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-12-27 23:28:24 +0500

    feat(storybook): Storybook for React Native added

---
+++ App.js
@@ -0,0 +1,29 @@
+export default from './storybook';
+
+// NOTE: The code below is what CRNA generates out of the box. We currently
+// have no clever way of replacing this with Storybook's UI (Vanilla RN does!)
+// so for now we just replace the code outright. Keeping this here for clarity.
+//
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+//
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
