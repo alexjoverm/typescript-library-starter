e5b6ace690822ab5ba04f2ef1e47427402053687
Matthew Davis <matthew@appsoa.io>
2018-01-24 12:04:48 -0600

    bump (#1)
    * chore(package): update jest to version 22.0.2
    
    * chore(package): update semantic-release to version 11.0.2
    
    * chore(package): update rollup to version 0.53.0
    
    * Improving TypeScript compilation
    
    * fix the incorrect cli command
    
    * chore(package): update @types/jest to version 22.0.0

--- tools/semantic-release-prepare.ts
+++ tools/semantic-release-prepare.ts
@@ -25,7 +25,7 @@ console.log()
 if (pkg.repository.url.trim()) {
   console.log(colors.cyan("Now run:"))
   console.log(colors.cyan("  npm install -g semantic-release-cli"))
-  console.log(colors.cyan("  semantic-release setup"))
+  console.log(colors.cyan("  semantic-release-cli setup"))
   console.log()
   console.log(
     colors.cyan('Important! Answer NO to "Generate travis.yml" question')
@@ -44,7 +44,7 @@ if (pkg.repository.url.trim()) {
   )
   console.log(colors.cyan("Then run:"))
   console.log(colors.cyan("  npm install -g semantic-release-cli"))
-  console.log(colors.cyan("  semantic-release setup"))
+  console.log(colors.cyan("  semantic-release-cli setup"))
   console.log()
   console.log(
     colors.cyan('Important! Answer NO to "Generate travis.yml" question')
