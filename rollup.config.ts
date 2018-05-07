0dda8312dfc147e97a6acce6e192e1d0773cb2ee
Peerapong Maitriwong <dev.peerapong@gmail.com>
2018-05-07 23:19:42 +0700

    specify sourcemap per file instead

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,10 +12,9 @@ const libraryName = '--libraryname--'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd' },
-    { file: pkg.module, format: 'es' },
+    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
   ],
-  sourcemap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
