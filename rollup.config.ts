3e3e43222b3a13dcf345dd3b7e3dd490147a4363
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-02-09 12:08:23 -0200

    style: run prettier on all files
    to reproduce this commit:
    - yarn install --ignore-scripts
    - rm yarn.lock
    - yarn prettier '**/**' # ignore parser errors

--- rollup.config.ts
+++ rollup.config.ts
@@ -13,12 +13,12 @@ export default {
   input: `src/${libraryName}.ts`,
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
-    include: 'src/**',
+    include: 'src/**'
   },
   plugins: [
     // Allow json resolution
@@ -33,6 +33,6 @@ export default {
     resolve(),
 
     // Resolve source maps to the original source
-    sourceMaps(),
-  ],
+    sourceMaps()
+  ]
 }
