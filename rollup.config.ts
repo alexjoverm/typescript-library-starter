a5c12c2ba86c0450812434e80d3c9fc50fcb8247
Muhammad Ubaid Raza <mubaidr@gmail.com>
2020-02-14 19:12:40 +0500

    replace tslint with eslint

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,9 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
+import commonjs from '@rollup/plugin-commonjs'
 import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import resolve from 'rollup-plugin-node-resolve'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import typescript from 'rollup-plugin-typescript2'
 
 const pkg = require('./package.json')
 
@@ -13,12 +13,12 @@ export default {
   input: `src/${libraryName}.ts`,
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
-    include: 'src/**',
+    include: 'src/**'
   },
   plugins: [
     // Allow json resolution
@@ -33,6 +33,6 @@ export default {
     resolve(),
 
     // Resolve source maps to the original source
-    sourceMaps(),
-  ],
+    sourceMaps()
+  ]
 }
