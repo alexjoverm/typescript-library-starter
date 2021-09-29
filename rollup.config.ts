f4eaab3846643dd055d120d6f679db3d42ed3dfc
AleBles <a.bles@azerion.com>
2021-09-29 20:29:25 +0200

    * Updated typedoc/typescript/rollup * Switched from rollup-plugin-typescript2 to rollup-plugin-terser

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import { terser } from 'rollup-plugin-terser'
+import json from '@rollup/plugin-json'
 
 const pkg = require('./package.json')
 
@@ -12,8 +11,7 @@ const libraryName = '--libraryname--'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: libraryName, format: 'esm', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
@@ -24,7 +22,7 @@ export default {
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    terser(),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
