e2856593752a2b94559487d8f50d715afde29fd5
AleBles <a.bles@azerion.com>
2021-09-29 21:12:40 +0200

    Set up ESlint

--- test/library.test.ts
+++ test/library.test.ts
@@ -3,12 +3,12 @@ import DummyClass from "../src/--libraryname--"
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
     expect(true).toBeTruthy()
   })
 
-  it("DummyClass is instantiable", () => {
+  it('DummyClass is instantiable', () => {
     expect(new DummyClass()).toBeInstanceOf(DummyClass)
   })
 })
